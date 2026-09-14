ad6bd122fec6a74dcd85d0264bb74f9b71fe95b2
Bhansen9 <Benjamin.t.hansen@icloud.com>
2026-09-13 20:36:45 -0500

    Finished project 1

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -2,10 +2,23 @@ package edu.luc.cs.laufer.cs371.shapes
 
 // TODO: implement this behavior
 
+
 import Shape.*
 
 object boundingBox:
   def apply(s: Shape): Location = s match
-    case _ => Location(0, 0, Rectangle(0, 0)) // not yet implemented
+    case Rectangle(width, height) =>
+      Location(0, 0, Rectangle(width, height))
+
+    case Location(x, y, shape) =>
+      val Location(boxX, boxY, rectangle) = apply(shape)
+      Location(x + boxX, y + boxY, rectangle)
+    
+    case Ellipse(width, height) =>
+      Location(-width, -height, Rectangle(width * 2, height * 2))
+
+    case Group(shapes*) =>
+      Location(0,0, Rectangle(0,0))
+
 
 end boundingBox
